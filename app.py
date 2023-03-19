5691eafb267fd86e9562992d2e04e2f66fa3e66f
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-19 20:39:08 +0530

    added another route

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ PORT = 5000
 def hello_world():
     return render_template('index.html')
 
+@app.route('/products')
+def products():
+    return 'Products Page'
+
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
